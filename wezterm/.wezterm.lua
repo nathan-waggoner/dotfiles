8c906ed463f32923b0a58d7aaf0be30cd05395bb
Nathan Waggoner <nathan@nathanwaggoner.com>
2025-07-13 19:31:51 -0400

    Updating dark mode switch

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,13 +1,19 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local appearance = wezterm.gui.get_appearance()
+local appearance = ""
 
-if appearance:find("Dark") then
+if wezterm.gui.get_appearance():find("Dark") then
+	appearance = "dark"
 	config.color_scheme = "nightfox"
 else
+	appearance = "light"
 	config.color_scheme = "dayfox"
 end
 
+if wezterm.GLOBAL.appearance ~= appearance then
+	wezterm.GLOBAL.appearance = appearance
+	wezterm.background_child_process({ "/bin/zsh", "-c", "/opt/homebrew/bin/tmux source-file ~/.tmux.conf" })
+end
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
